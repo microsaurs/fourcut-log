fe668d6c0d981cb35f0338b5d0c327939da9645f
microsaurs <microsaurs@gmail.com>
2025-03-17 22:28:36 +0900

    init

---
+++ src/test/java/com/flog/fourcut_log/FourcutLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flog.fourcut_log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourcutLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
